55a1dcbc25fcb1fd17eb3689ee20211abe3561c6
Nacchamu <china.natsuki@gmail.com>
2017-05-20 20:28:21 +0900

    Install&setting ckeditor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,22 @@ class ArticlesController < ApplicationController
 
   def show
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    article = Article.new(create_params)
+    if article.save
+      redirect_to articles_path
+    else
+      redirect_to new_article_path
+    end
+  end
+
+  private
+  def create_params
+    params.require(:article).permit(:title, :text, :thumbnail).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  mount Ckeditor::Engine => '/ckeditor'
   root 'articles#index'
-  resources :articles, only:[:index, :show]
+  get 'inquiry' => 'inquiry#index'              # 入力画面
+  post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+  post 'inquiry/thanks' => 'inquiry#thanks'     # 送信完了画面
+  resources :articles, only:[:index, :show, :new, :create]
 end
